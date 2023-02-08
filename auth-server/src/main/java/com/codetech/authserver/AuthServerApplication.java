9517583f2c560a887585b0653dc0e2ed35ccaaaf
ibrahim <ibrahim.20092010@gmail.com>
2023-02-08 03:05:29 +0300

    application class fixed

--- auth-server/src/main/java/com/codetech/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/codetech/authserver/AuthServerApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import com.codetech.authserver.controller.LoginController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses=LoginController.class)
+@ComponentScan(basePackageClasses = LoginController.class)
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
