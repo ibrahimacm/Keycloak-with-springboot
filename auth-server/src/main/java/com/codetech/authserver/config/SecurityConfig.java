67fee71b48211442af57a7d05e3612d8c431347b
ibrahim <ibrahim.20092010@gmail.com>
2023-02-08 03:57:05 +0300

    application class fixed

--- auth-server/src/main/java/com/codetech/authserver/config/SecurityConfig.java
+++ auth-server/src/main/java/com/codetech/authserver/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	
 		super.configure(http);
-		http.csrf().disable().authorizeRequests().antMatchers("/auth/*").hasRole("ADMIN")
+		http.csrf().disable().authorizeRequests().antMatchers("/auth/*")
 			                .anyRequest().permitAll();
 	}
 
