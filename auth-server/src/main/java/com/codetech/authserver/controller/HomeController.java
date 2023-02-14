3922ca6631528d930e5db8dc15bfdb207ebdd5da
ibrahim <ibrahim.20092010@gmail.com>
2023-02-14 04:42:24 +0300

    .jsp added

--- auth-server/src/main/java/com/codetech/authserver/controller/HomeController.java
+++ auth-server/src/main/java/com/codetech/authserver/controller/HomeController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@RequestMapping("/home")
 	public String homePage() {
-		return "home";
+		return "home.jsp";
 	}
 }
