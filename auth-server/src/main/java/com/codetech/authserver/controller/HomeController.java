dee0d13ff17a053dcffb431d3a934609e2015893
ibrahim <ibrahim.20092010@gmail.com>
2023-02-14 04:26:55 +0300

    edit controller name

--- auth-server/src/main/java/com/codetech/authserver/controller/HomeController.java
+++ auth-server/src/main/java/com/codetech/authserver/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class HomeController {
 	@RequestMapping("/home")
 	public String homePage() {
 		return "home";
