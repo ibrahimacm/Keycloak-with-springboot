7cc3c01bd2a2dd3da081a0e246ffce6f76a53d0f
ibrahim <ibrahim.20092010@gmail.com>
2023-02-14 04:05:40 +0300

    add jsp page

--- auth-server/src/main/java/com/codetech/authserver/controller/LoginController.java
+++ auth-server/src/main/java/com/codetech/authserver/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.codetech.authserver.controller;
 
+import org.springframework.stereotype.Controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +51,9 @@ public class LoginController {
 	public String getProducts(){
 		return "products";
 	}
-
+	
+	@RequestMapping("/home")
+	public String homePage() {
+		return "home";
+	}
 }
