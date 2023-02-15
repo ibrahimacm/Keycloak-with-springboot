d80f912bd6407a28019db6400c11895f50932a81
ibrahim <ibrahim.20092010@gmail.com>
2023-02-15 19:16:39 +0300

    edit on pom file and Application main class

--- auth-server/src/main/java/com/codetech/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/codetech/authserver/AuthServerApplication.java
@@ -3,12 +3,17 @@ package com.codetech.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class AuthServerApplication {
-
+public class AuthServerApplication extends SpringBootServletInitializer {
+	@Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(AuthServerApplication.class);
+        }
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
